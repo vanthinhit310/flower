cba0a6159df00fbe770ac2105342c0d52947fd5c
Anonymous <vanthinhit310@gmail.com>
2020-02-01 19:14:17 +0700

    -

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Ruby Wedding Deco</title>
+
+    @yield('style')
+</head>
+<body>
+<div id="app">
+    <section class="main-wrapper">
+        @yield('content')
+    </section>
+</div>
+</body>
+@yield('script')
+@include('layouts.alert')
+</html>
